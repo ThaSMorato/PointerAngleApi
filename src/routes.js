101757a1ff376fae5c45a9122005739cdf442f47
ThaSMorato <tha.s.morato@gmail.com>
2021-01-10 11:14:19 -0300

    quick fix url and version

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const ClockController = require('./controller/ClockController');
 
 const routes = Router();
 
-routes.get('/api/rest/clock/:hour/:minute?', ClockController.middlewareValidateHourAndMinutes, ClockController.AngleBetweenHourAndMinute)
+routes.get('/v1.0.1/rest/clock/:hour/:minute?', ClockController.middlewareValidateHourAndMinutes, ClockController.AngleBetweenHourAndMinute)
 
 
 module.exports = routes;
